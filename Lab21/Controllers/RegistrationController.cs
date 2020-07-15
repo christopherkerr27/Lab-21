17763ac8e66918e021d54ad47b94a2465af7f630
Christopher Kerr <christopherke@truck-hero.com>
2020-07-15 18:29:07 -0400

    made changes

---
+++ Lab21/Controllers/RegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab21.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Registration registration)
+        {
+            // Save to a database??? 
+            // Typically you would do something with this...
+            // But for now we will just display the results
+
+            return View(registration);
+        }
+    }
+}
